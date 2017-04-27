7bb221b3d4b337abf45be148bf17553cf4e7dfb3
VladVons <vladvons@gmail.com>
2017-04-27 12:02:47 +0300

    just commit

--- app/src/main/java/ua/com/vando/apk_airplan/ActivityDialogMotorDC.java
+++ app/src/main/java/ua/com/vando/apk_airplan/ActivityDialogMotorDC.java
@@ -3,11 +3,19 @@ package ua.com.vando.apk_airplan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityDialogMotorDC extends AppCompatActivity {
+import com.hrules.horizontalnumberpicker.HorizontalNumberPicker;
+import com.hrules.horizontalnumberpicker.HorizontalNumberPickerListener;
+
+public class ActivityDialogMotorDC extends AppCompatActivity implements HorizontalNumberPickerListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_motor_dc);
+    }
+
+    @Override
+    public void onHorizontalNumberPickerChanged(HorizontalNumberPicker horizontalNumberPicker, int value) {
+
     }
 }
